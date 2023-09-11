108e6218991d485e2a5e5390c5b3333f81461c46
貓宅 <do.rubbish@gmail.com>
2023-09-12 00:14:50 +0800

    刪除專案 keystore，才用 local.propertie 來指定路徑，build.gradle 可讀取 local.propertie 參數

--- App.js
+++ App.js
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, { useState, useEffect } from 'react'
 import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme, View } from 'react-native'
 import codePush from 'react-native-code-push'
@@ -12,25 +5,48 @@ import { Colors, Header } from 'react-native/Libraries/NewAppScreen'
 
 function App() {
 
-  const [message, setMessage] = useState('正在检查更新...')
+  const [message, setMessage] = useState('正在檢查更新...')
+  const [showProgress, setShowProgress] = useState('0%')
 
-  const checkUpdate = () => {
-    codePush.checkForUpdate(codePush.CheckFrequency.ON_APP_START)
-      .then((update) => {
-        if (update) {
-          setMessage('有新的更新！')
+  useEffect(() => {
+    codePush.checkForUpdate()
+      .then((remotePackage) => {
+        if (remotePackage) {
+          setMessage(`有新的更新！Version ${remotePackage.appVersion} (${(remotePackage.packageSize / 1024 / 1024).toFixed(2)}mb) is available for download`)
+          syncInNonSilent(remotePackage)
         } else {
           setMessage('已是最新，不需要更新！')
         }
-      }).catch((error) => {
-        console.log({ error })
-      });
-  }
-
-  useEffect(() => {
-    checkUpdate()
-    console.log('App.js')
+      })
+      .catch((error) => console.log(error))
   }, []);
+
+  const syncInNonSilent = (remotePackage) => {
+    console.info('安裝更新並立刻重啟應用')
+    codePush.disallowRestart() // 禁止重啟
+    remotePackage
+      .download(({receivedBytes, totalBytes}) => {
+        setMessage('開始下載')
+        let downloadProgress = (receivedBytes / totalBytes) * 100;
+        setShowProgress(downloadProgress.toFixed(2) + "%")
+      })
+      .then((localPackage) => {
+        // 下載完成了，呼叫這個方法
+        setMessage('開始安裝')
+        localPackage.install(codePush.InstallMode.IMMEDIATE).then(() => {
+          setMessage('安裝完成')
+          codePush.notifyAppReady()
+          codePush.allowRestart() // 強制更新
+          codePush.restartApp(true)
+        }).catch((error) => {
+          console.log(error)
+          setMessage('更新出錯，請聯繫管理員！')
+        })
+      }).catch((error) => {
+        console.log(error)
+        setMessage('更新出錯，請聯繫管理員！')
+      })
+  }
 
   const isDarkMode = useColorScheme() === 'dark'
 
@@ -48,9 +64,10 @@ function App() {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-          <Text style={{ fontSize: 20, textAlign: 'center' }}>版本号 1.0</Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1, padding: 10 }}>
+          <Text style={{ fontSize: 20, textAlign: 'center' }}>版本號 1.0.0</Text>
           <Text style={{ fontSize: 20, textAlign: 'center' }}>{message}</Text>
+          <Text style={{ fontSize: 20, textAlign: 'center' }}>{showProgress}</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
